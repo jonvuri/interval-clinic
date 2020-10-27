173c99cbe4a589df019c30850e7fc04748805dee
Jonathan Rajavuori <jrajav@gmail.com>
2020-10-26 20:34:12 -0500

    Fill out Minor Third references

--- src/components/reference/MinorThird.tsx
+++ src/components/reference/MinorThird.tsx
@@ -2,10 +2,145 @@ import Base from './Base'
 
 const MinorThird = Base('m3', {
   ascending: {
-    Pop: [],
+    Pop: [
+      {
+        artist: 'The White Stripes',
+        title: 'Seven Nation Army',
+        description: 'First two notes of the bass riff.',
+        youtubeId: 'y9ANOzmSKQg',
+        youtubeStart: 0,
+      },
+      {
+        artist: 'Deep Purple',
+        title: 'Smoke on the Water',
+        description: 'First two notes of the guitar riff.',
+        youtubeId: 'zUwEIt9ez7M',
+        youtubeStart: 0,
+      },
+      {
+        artist: 'Black Sabbath',
+        title: 'Iron Man',
+        description: 'First two notes of the guitar riff.',
+        youtubeId: 'BwoHuf3eJQg',
+        youtubeStart: 28,
+      },
+    ],
+    Folk: [
+      {
+        artist: 'Johannes Brahms',
+        title: 'Lullaby',
+        description: 'First two notes of the melody.',
+        youtubeId: 'T6nb35I9w-8',
+        youtubeStart: 13,
+        youtubeNote: 'Performed by Yo-Yo Ma and Kathryn Stott',
+      },
+      {
+        artist: '(Traditional)',
+        title: 'Greensleeves',
+        description: 'First two notes of the melody.',
+        youtubeId: 'r4HKfHLU21k',
+        youtubeStart: 0,
+        youtubeNote: 'Performed by David Nevue',
+      },
+    ],
+    Media: [
+      {
+        artist: 'Richard Rodgers',
+        title: 'So Long Farewell from The Sound of Music',
+        description: 'First two notes of the melody: SO LONG, farewell...',
+        youtubeId: 'Qy9_lfjQopU',
+        youtubeStart: 0,
+      },
+      {
+        artist: 'Paul Webster, Bob Harris',
+        title: 'Spiderman Theme',
+        description: 'First two notes of the melody: SPIDERman, Spiderman...',
+        youtubeId: 'SUtziaZlDeE',
+        youtubeStart: 10,
+      },
+      {
+        artist: 'Nobuo Uematsu',
+        title: "Aerith's Theme",
+        description: 'First two notes of the melody.',
+        youtubeId: 'MdsJ9nczMuA',
+        youtubeStart: 0,
+      },
+    ],
+    Jazz: [
+      {
+        artist: 'Django Reinhardt',
+        title: 'Minor Swing',
+        description: 'First two notes of the melody.',
+        youtubeId: 'DL5aUq1_7N0',
+        youtubeStart: 0,
+      },
+    ],
+    'Western Classical': [
+      {
+        artist: 'Erik Satie',
+        title: 'Gymnopédie No. 1',
+        description: 'First two notes of the melody.',
+        youtubeId: 'S-Xm7s9eGxU',
+        youtubeStart: 0,
+        youtubeNote: 'Performed by Daniel Varsano and Philippe Entremont',
+      },
+    ],
   },
   descending: {
-    Pop: [],
+    Pop: [
+      {
+        artist: 'The Beatles',
+        title: 'Hey Jude',
+        description: 'First two notes of the melody: HEY JUDE...',
+        youtubeId: 'mQER0A0ej0M',
+        youtubeStart: 0,
+      },
+    ],
+    Folk: [
+      {
+        artist: 'Walter Rollins, Steve Nelson',
+        title: 'Frosty the Snowman',
+        description: 'First two notes of the melody: FROSTY the snowman...',
+        youtubeId: '5Pl2k-K4ehs',
+        youtubeStart: 16,
+        youtubeNote: 'Performed by Gene Autry',
+      },
+    ],
+    Media: [
+      {
+        artist: 'Elton John, Tim Rice',
+        title: 'Can You Feel the Love Tonight?',
+        description: 'First two notes of the chorus melody: CAN YOU feel...',
+        youtubeId: 'KjgWWjkNbhU',
+        youtubeStart: 48,
+      },
+    ],
+    Jazz: [
+      {
+        artist: 'Erroll Garner',
+        title: 'Misty',
+        description: 'First two notes of the melody.',
+        youtubeId: 'AA2_6kS45E0',
+        youtubeStart: 0,
+      },
+      {
+        artist: 'Chick Corea',
+        title: '500 Miles High',
+        description: 'First two notes of the melody: SOMEDAY...',
+        youtubeId: 'UD21FG2Nmcw',
+        youtubeStart: 31,
+      },
+    ],
+    'Western Classical': [
+      {
+        artist: 'Edvard Grieg',
+        title: 'Morning Mood',
+        description: 'First two notes of the melody.',
+        youtubeId: '-rh8gMvzPw0',
+        youtubeStart: 0,
+        youtubeNote: 'Performed by the Berlin Philharmonic orchestra',
+      },
+    ],
   },
 })
 
