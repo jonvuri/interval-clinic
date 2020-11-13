d741afd404006563ae7a0431ec929bcce2c7b38a
Jonathan Rajavuori <jrajav@gmail.com>
2020-11-13 00:08:09 -0600

    Tweak TuningToggle styling

--- src/components/TuningToggle.tsx
+++ src/components/TuningToggle.tsx
@@ -22,44 +22,47 @@ const JustToggle = ({ just, onChange }: Props) => {
 
   return (
     <div className={styles.container}>
-      <div
-        className={styles.twelveContainer}
-        style={{
-          stroke: just ? '#555' : '#aaa',
-          color: just ? '#555' : '#aaa',
-        }}
-      >
-        <div className={styles.inner}>12TET</div>
-        <div className={styles.inner}>
-          <svg width="72" height="63" viewBox="-2 -2 76 67">
-            <path
-              onClick={handleTwelveClick}
-              strokeWidth="4px"
-              strokeLinejoin="round"
-              strokeLinecap="round"
-              d="M0 31.17691453623979L18 0L54 0L72 31.17691453623979L54 62.35382907247958L18 62.35382907247958Z"
-            ></path>
-          </svg>
+      <div className={styles.innerContainer}>
+        <div className={styles.label}>Tuning</div>
+        <div
+          className={styles.twelveContainer}
+          style={{
+            stroke: just ? '#555' : '#ccc',
+            color: just ? '#555' : '#ccc',
+          }}
+        >
+          <div className={styles.inner}>12TET</div>
+          <div className={styles.inner}>
+            <svg width="72" height="63" viewBox="-2 -2 76 67">
+              <path
+                onClick={handleTwelveClick}
+                strokeWidth="4px"
+                strokeLinejoin="round"
+                strokeLinecap="round"
+                d="M0 31.17691453623979L18 0L54 0L72 31.17691453623979L54 62.35382907247958L18 62.35382907247958Z"
+              ></path>
+            </svg>
+          </div>
         </div>
-      </div>
-      <div
-        className={styles.justContainer}
-        style={{
-          stroke: just ? '#aaa' : '#555',
-          color: just ? '#aaa' : '#555',
-        }}
-      >
-        <div className={styles.inner}>Just</div>
-        <div className={styles.inner}>
-          <svg width="72" height="63" viewBox="-2 -2 76 67">
-            <path
-              onClick={handleJustClick}
-              strokeWidth="4px"
-              strokeLinejoin="round"
-              strokeLinecap="round"
-              d="M0 31.17691453623979L18 0L54 0L72 31.17691453623979L54 62.35382907247958L18 62.35382907247958Z"
-            ></path>
-          </svg>
+        <div
+          className={styles.justContainer}
+          style={{
+            stroke: just ? '#ccc' : '#555',
+            color: just ? '#ccc' : '#555',
+          }}
+        >
+          <div className={styles.inner}>Just</div>
+          <div className={styles.inner}>
+            <svg width="72" height="63" viewBox="-2 -2 76 67">
+              <path
+                onClick={handleJustClick}
+                strokeWidth="4px"
+                strokeLinejoin="round"
+                strokeLinecap="round"
+                d="M0 31.17691453623979L18 0L54 0L72 31.17691453623979L54 62.35382907247958L18 62.35382907247958Z"
+              ></path>
+            </svg>
+          </div>
         </div>
       </div>
     </div>
